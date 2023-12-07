d8a615a063a4a25de41178de73bb54885cf99187
Air-ljh8 <ljh8@torder.com>
2023-12-07 15:29:56 +0900

    [feat] TopNavBar 컴포넌트 구현

---
+++ src/containers/TopNavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header
+    class="relative flex h-16 w-full flex-col items-center justify-between"
+  >
+    <div
+      class="flex h-16 w-full items-center justify-between border-gray_02-light p-4 dark:border-gray_02-dark"
+    >
+      <Logo />
+      <ThemeSwitcher />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import Logo from '@src/components/Logo.vue';
+import ThemeSwitcher from '@src/components/ThemeSwitcher.vue';
+</script>
