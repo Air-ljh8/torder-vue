de3162eab208a6282746aadbd4f5ae663a3bf2e4
Air-ljh8 <ljh8@torder.com>
2023-12-07 15:27:44 +0900

    [feat] App 컴포넌트에서 theme, toggleTheme provide 적용

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <RouterView />
+  <RouterView :class="theme" />
 </template>
+<script setup lang="ts">
+import { provide } from 'vue';
+import { useTheme } from '@src/composables/useTheme';
+
+const { theme, toggleTheme } = useTheme();
+
+provide('theme', theme);
+provide('toggleTheme', toggleTheme);
+</script>
