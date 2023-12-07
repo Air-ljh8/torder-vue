ca6e95d92aa7e182daf7a37902a30af3d5316c9d
Air-ljh8 <ljh8@torder.com>
2023-12-07 14:48:21 +0900

    [feat] Logo 컴포넌트 구현

---
+++ src/components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div className="flex items-center justify-between gap-[6px]">
+    <img
+      src="/src/assets/logo/character.png"
+      alt="베스트라빈스 캐릭터"
+      width="36"
+      height="36"
+    />
+    <img
+      src="/src/assets/logo/title-light.png"
+      alt="베스트라빈스 타이틀"
+      width="120"
+      height="20"
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
