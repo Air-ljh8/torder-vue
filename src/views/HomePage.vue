f0014335c44f816d853754455a71bca247e17474
Air-ljh8 <ljh8@torder.com>
2023-12-08 12:52:24 +0900

    [feat] HomePage에 GlobalNavBar 추가

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,10 +4,12 @@
       class="mx-auto flex h-full flex-col items-center justify-between bg-gray_00-light text-gray_05-light dark:bg-gray_00-dark dark:text-gray_05-dark sm:w-screen md:w-96"
     >
       <TopNavBar />
+      <GlobalNavBar />
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
 import TopNavBar from '@containers/TopNavBar.vue';
+import GlobalNavBar from '@containers/GlobalNavBar.vue';
 </script>
