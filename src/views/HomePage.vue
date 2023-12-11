a8c92fe84da3b4e09e3684c44d7c5cdb5f4f328c
Air-ljh8 <ljh8@torder.com>
2023-12-11 17:00:16 +0900

    [feat] HomePage에 Carousel 컴포넌트 적용
    - 추후 로딩, 에러 처리

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,10 +4,17 @@
       class="mx-auto flex h-full flex-col items-center justify-between bg-gray_00-light text-gray_05-light dark:bg-gray_00-dark dark:text-gray_05-dark sm:w-screen md:w-96"
     >
       <TopNavBar />
+      <div v-if="!data?.recommendations">로딩중입니다.</div>
+      <Carousel :recommendations="data?.recommendations" v-else />
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
 import TopNavBar from '@containers/TopNavBar.vue';
+import Carousel from '@containers/Carousel.vue';
+
+import { useGetRecommendations } from '@apis/recommendations';
+
+const { data } = useGetRecommendations();
 </script>
