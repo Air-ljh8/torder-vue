bedc3d19fbf5e53ce21d97058b6ca96be57b9c74
Air-ljh8 <ljh8@torder.com>
2023-12-11 12:43:55 +0900

    [feat] SizePage에 ProgressBar 적용

--- src/views/SizePage.vue
+++ src/views/SizePage.vue
@@ -1,9 +1,13 @@
 <template>
-  <main>
-    <TheWelcome content="Size Page" />
-  </main>
+  <div class="h-screen w-screen bg-gray_01-light dark:bg-gray_01-dark">
+    <section
+      class="mx-auto flex h-full flex-col items-center justify-between bg-gray_00-light text-gray_05-light dark:bg-gray_00-dark dark:text-gray_05-dark sm:w-screen md:w-96"
+    >
+      <ProgressNavBar prevPage="/" pageName="size" />
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
-import TheWelcome from '@components/TheWelcome.vue';
+import ProgressNavBar from '@src/containers/ProgressNavBar.vue';
 </script>
