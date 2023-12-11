17e6c3196fccd97ba396ec529e0f7bdda2d48c70
Air-ljh8 <ljh8@torder.com>
2023-12-11 12:21:45 +0900

    [feat] ProgressNavBar 컨테이너 구현

---
+++ src/containers/ProgressNavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header
+    class="relative flex h-16 w-full flex-col items-center justify-between"
+  >
+    <div
+      class="flex h-16 w-full items-center justify-between border-gray_02-light p-4 dark:border-gray_02-dark"
+    >
+      <BackButton :prevPage="prevPage" />
+      <CloseButton />
+    </div>
+    <div
+      class="absolute bottom-0 h-[2px] bg-gray_05-light dark:bg-gray_05-dark left-0"
+      :class="processWidth[pageName]"
+    ></div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import BackButton from '@src/components/BackButton.vue';
+import CloseButton from '@src/components/CloseButton.vue';
+
+const processWidth = {
+  size: 'w-1/3',
+  ingredient: 'w-2/3',
+  result: 'w-full',
+};
+
+const { prevPage } = defineProps<{
+  pageName: 'size' | 'ingredient' | 'result';
+  prevPage: string;
+}>();
+</script>
