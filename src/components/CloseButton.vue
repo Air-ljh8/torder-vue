c017c0fea0a792d77202aeb60d2d8882e1c89c1a
Air-ljh8 <ljh8@torder.com>
2023-12-11 12:13:07 +0900

    [feat] CloseButton 컴포넌트 구현

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterLink to="/">
+    <div class="text-gray_05-light dark:text-gray_05-dark w-6 h-6">종</div>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
