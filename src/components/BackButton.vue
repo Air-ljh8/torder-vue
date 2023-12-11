07eb015ce42f489cb5ef96da4442acb3eb36d06b
Air-ljh8 <ljh8@torder.com>
2023-12-11 12:12:54 +0900

    [feat] BackButton 컴포넌트 구현

---
+++ src/components/BackButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterLink :to="prevPage">
+    <div class="text-gray_05-light dark:text-gray_05-dark w-6 h-6">뒤</div>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+const { prevPage } = defineProps<{
+  prevPage: string;
+}>();
+</script>
