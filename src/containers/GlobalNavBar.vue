2265035fc067c6f5078ce22a9d5c13d43126eb31
Air-ljh8 <ljh8@torder.com>
2023-12-11 11:35:21 +0900

    [edit] GlobalNavBar 스타일 로직 수정
    - 코드리뷰 반영

--- src/containers/GlobalNavBar.vue
+++ src/containers/GlobalNavBar.vue
@@ -3,40 +3,12 @@
     class="flex h-20 w-full flex-row justify-around border-t border-t-gray_01-light p-2 pb-8 dark:border-t-gray_01-dark"
   >
     <RouterLink to="/" class="flex flex-col items-center justify-start">
-      <div
-        :class="[
-          'h-6 w-6',
-          isHome ? 'bg-primary-light' : 'bg-gray_04-light dark:bg-gray_03-dark',
-        ]"
-      ></div>
-      <p
-        :class="[
-          'text-x_small',
-          isHome
-            ? 'text-primary-light'
-            : 'text-gray_04-light dark:text-gray_03-dark',
-        ]"
-      >
-        홈
-      </p>
+      <div :class="getIconStyle(isHome)"></div>
+      <p :class="getTextStyle(isHome)">홈</p>
     </RouterLink>
     <RouterLink to="/user" class="flex flex-col items-center justify-start">
-      <div
-        :class="[
-          'h-6 w-6',
-          isUser ? 'bg-primary-light' : 'bg-gray_04-light dark:bg-gray_04-dark',
-        ]"
-      ></div>
-      <p
-        :class="[
-          'text-x_small',
-          isUser
-            ? 'text-primary-light'
-            : 'text-gray_04-light dark:text-gray_03-dark',
-        ]"
-      >
-        마이
-      </p>
+      <div :class="getIconStyle(isUser)"></div>
+      <p :class="getTextStyle(isUser)">마이</p>
     </RouterLink>
   </footer>
 </template>
@@ -47,6 +19,17 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 const isHome = route.path === '/';
 const isUser = route.path === '/user';
+
+const getIconStyle = (isPath: boolean) => ({
+  'h-6 w-6': true,
+  'bg-primary-light': isPath,
+  'bg-gray_04-light dark:bg-gray_03-dark': !isPath,
+});
+const getTextStyle = (isPath: boolean) => ({
+  'text-x_small': true,
+  'text-primary-light': isPath,
+  'text-gray_04-light dark:text-gray_03-dark': !isPath,
+});
 </script>
 
 <style scoped></style>
