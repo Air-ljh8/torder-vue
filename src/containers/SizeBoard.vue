bb118a7d5cbc5f5d36f3d3a4e6cb74b3101573c0
Air-ljh8 <ljh8@torder.com>
2023-12-12 16:24:55 +0900

    [feat] SizeBoard 컨테이너 구현

---
+++ src/containers/SizeBoard.vue
@@ -0,0 +1,19 @@
+<template>
+  <main
+    class="flex h-full w-full flex-1 flex-col justify-start gap-y-5 px-5 pt-[34px]"
+  >
+    <h2 class="text-x_large font-semi_bold">어떤 사이즈를 드시나요?</h2>
+    <SizeRow title="1가지 맛" :sizes="sizes.slice(0, 2)" />
+    <SizeRow title="2가지 맛" :sizes="sizes.slice(2, 4)" />
+    <SizeRow title="3가지 맛 이상" :sizes="sizes.slice(4, sizes.length)" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import SizeRow from '@components/SizeRow.vue';
+import { Size } from '@interface/goods';
+
+defineProps<{
+  sizes: Size[];
+}>();
+</script>
