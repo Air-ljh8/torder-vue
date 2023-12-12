455d05dcd7b640d9a23d52c5af288855bfb73f80
Air-ljh8 <ljh8@torder.com>
2023-12-12 16:19:06 +0900

    [feat] SizeRow 컴포넌트 구현

---
+++ src/components/SizeRow.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex flex-col gap-y-2">
+    <p class="text-medium font-semi_bold">{{ title }}</p>
+    <div class="flex gap-x-3">
+      <button
+        v-for="size in sizes"
+        :key="size.id"
+        class="flex flex-col items-center justify-between gap-y-[10px]"
+        @click="
+          () => {
+            handleSizeClick(size);
+          }
+        "
+      >
+        <div :class="getUserSelectStyle(size)">
+          <img
+            :src="size.imageUrl"
+            :alt="size.name"
+            :width="getImageWidth(size)"
+            :height="30"
+          />
+        </div>
+        <p class="text-center text-small">{{ size.name }}</p>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Size } from '@interface/goods';
+import { useUserSelectStore } from '@store/storeUserSelect';
+
+defineProps<{
+  title: string;
+  sizes: Size[];
+}>();
+
+const getImageWidth = (size: Size) => {
+  if (size.value <= 2) return 30;
+  if (size.value === 3) return 24;
+  return 74;
+};
+
+const { userSelect, setUserSelectSizeId, setUserSelectSizeValue } =
+  useUserSelectStore();
+
+const handleSizeClick = (size: Size) => {
+  if (userSelect.sizeId === size.id) {
+    setUserSelectSizeId(0);
+    setUserSelectSizeValue(0);
+    return;
+  }
+  setUserSelectSizeId(size.id);
+  setUserSelectSizeValue(size.value);
+};
+
+const getUserSelectStyle = (size: Size) => ({
+  'flex h-[74px] w-[74px] items-center justify-center rounded-2xl border-[3px] bg-gray_01-light dark:bg-gray_01-dark':
+    true,
+  'border-secondary-light': userSelect.sizeId === size.id,
+  'border-gray_01-light dark:border-gray_01-dark':
+    userSelect.sizeId !== size.id,
+});
+</script>
+
+<style scoped></style>
