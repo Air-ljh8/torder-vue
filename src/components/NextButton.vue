eea7bd2bc975d2722b9cc4116e98e30476ac9b7a
Air-ljh8 <ljh8@torder.com>
2023-12-12 10:59:52 +0900

    [feat] NextButton 컴포넌트 구현

---
+++ src/components/NextButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="onClick" :class="getButtonStyle()"><slot></slot></button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  isPrimary?: boolean;
+  onClick: () => void;
+}>();
+
+const getButtonStyle = () => ({
+  'h-fit w-[340px] rounded-2xl border-0 p-6 text-large font-bold': true,
+  'bg-primary-light dark:bg-primary-dark': props.isPrimary,
+  'bg-gray_01-light dark:bg-gray_01-dark': !props.isPrimary,
+});
+</script>
