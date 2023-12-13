d7335c18b043b4c13da0b09ac710e083b5196a90
Air-ljh8 <ljh8@torder.com>
2023-12-13 16:39:49 +0900

    [feat] IngredientBoard 컨테이너 구현

---
+++ src/containers/IngredientBoard.vue
@@ -0,0 +1,59 @@
+<template>
+  <main
+    class="flex h-full w-full flex-1 flex-col justify-start gap-y-5 px-5 pt-[34px]"
+  >
+    <div class="flex flex-wrap gap-x-2">
+      <button
+        :key="ingredient.id"
+        class="flex flex-col items-center justify-between gap-y-[10px]"
+        @click="
+          () => {
+            handleIngredientClick(ingredient.id);
+          }
+        "
+        v-for="ingredient in ingredients"
+      >
+        <div :class="getUserSelectStyle(ingredient)">
+          <img
+            :src="ingredient.imageUrl ?? PLACE_HOLD_IMAGE_URL"
+            :alt="ingredient.name"
+            width="56"
+            height="56"
+            class="rounded-full"
+          />
+        </div>
+        <p class="text-center text-small">{{ ingredient.name }}</p>
+      </button>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { Ingredient } from '@src/interface/goods';
+import { useUserSelectStore } from '@src/store/storeUserSelect';
+import { PLACE_HOLD_IMAGE_URL } from '@src/constants';
+
+const { userSelect, setUserSelectIngredientIds } = useUserSelectStore();
+
+defineProps<{
+  ingredients: Ingredient[];
+}>();
+
+const handleIngredientClick = (id: number) => {
+  if (userSelect.ingredientIds.includes(id)) {
+    setUserSelectIngredientIds(
+      userSelect.ingredientIds.filter((ingredientId) => ingredientId !== id)
+    );
+    return;
+  }
+  setUserSelectIngredientIds([...userSelect.ingredientIds, id]);
+};
+
+const getUserSelectStyle = (ingredient: Ingredient) => ({
+  'flex h-fit w-fit items-center justify-center rounded-full border-[3px]':
+    true,
+  'border-secondary-light': userSelect.ingredientIds.includes(ingredient.id),
+  'border-gray_00-light dark:border-gray_00-dark':
+    !userSelect.ingredientIds.includes(ingredient.id),
+});
+</script>
