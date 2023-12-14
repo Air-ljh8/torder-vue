6e0673843ca23e8ad3644c00ab9fa13b98138a0d
Air-ljh8 <ljh8@torder.com>
2023-12-14 11:43:50 +0900

    [feat] ResultPage 구현
    - 로딩 기능 추가
    - RecipeCard 컴포넌트 적용

--- src/views/ResultPage.vue
+++ src/views/ResultPage.vue
@@ -4,7 +4,6 @@
       class="mx-auto flex h-full flex-col items-center justify-between bg-gray_00-light text-gray_05-light dark:bg-gray_00-dark dark:text-gray_05-dark sm:w-screen md:w-96"
     >
       <ProgressNavBar pageName="result" prevPage="/ingredient" />
-      <!-- <div v-if="isLoading">로딩중입니다.</div> -->
       <main
         class="flex h-full w-full flex-1 flex-col items-center justify-start gap-y-5 px-5 pt-[34px]"
       >
@@ -12,7 +11,8 @@
         <div
           class="relative flex h-[460px] w-[340px] items-center justify-center rounded-3xl shadow-light transition-all dark:shadow-dark"
         >
-          <!-- <RecipeCard :recipe='result' hasBookmark /> -->
+          <div v-if="isLoading">로딩중입니다.</div>
+          <RecipeCard v-else :recipe="data?.recipe!" hasBookmark />
         </div>
       </main>
       <NextButton :onClick="() => pushPage('home')">다시하기</NextButton>
@@ -21,12 +21,14 @@
 </template>
 
 <script setup lang="ts">
-import ProgressNavBar from '@src/containers/ProgressNavBar.vue';
-import RecipeCard from '@src/components/RecipeCard.vue';
-import NextButton from '@components/NextButton.vue';
-
-import { pushPage } from '@src/router/route.helper';
 import { useRoute } from 'vue-router';
+import ProgressNavBar from '@containers/ProgressNavBar.vue';
+import RecipeCard from '@components/RecipeCard.vue';
+import NextButton from '@components/NextButton.vue';
+import { pushPage } from '@router/route.helper';
+import { useGetRecipe } from '@src/apis/recipes';
 
 const route = useRoute();
+const { id } = route.params;
+const { data, isLoading } = useGetRecipe(Number(id));
 </script>
